fecff2bc5ed36e46ca54015bc9c4032049b064ab
Ambar Gorgon <ambargorgon@gmail.com>
2025-11-14 16:09:24 -0300

    replace lanyard for particles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,50 +1,53 @@
 import { useTranslation } from "react-i18next";
-import LanyardVercel from "./LanyardVercel.jsx"
-
+import Particles from "./Particles";
 
 const Hero = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="h-[100dvh] w-full flex flex-col md:flex-row items-center relative overflow-hidden">
-      {/* Lanyard */}
-      <div
-        className="
-    relative z-20 flex justify-center items-start
-    w-[300%] h-2/3
-    -translate-x-56
-    md:w-full md:translate-x-0 md:scale-100 md:h-full 
-    md:absolute md:inset-0 md:justify-center 
-  "
-      >
-        <LanyardVercel />
+    <div className="h-[100dvh] w-full relative overflow-hidden flex flex-col md:flex-row items-center">
+
+      {/* Particles as background */}
+      <div className="absolute inset-0 z-10">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={400}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
       </div>
 
-      {/* Texto */}
+      {/* Text */}
       <div
         className="
-      relative z-30 flex flex-col items-center text-center px-6 mb-20
-      md:items-start md:text-left md:px-20 md:mb-0
-    "
+          relative z-30 
+          flex flex-col items-center text-center justify-center
+          px-6 h-full
+          md:items-start md:text-left md:px-20
+        "
       >
-        <h1 className="font-title text-3xl md:text-5xl xl:text-8xl text-[#fff]">
+        <h1 className="font-title text-5xl md:text-5xl xl:text-8xl text-[#fff] leading-snug sm:text-center">
           {t("title")}
         </h1>
+
         <h6 className="mt-4 font-bold lowercase text-2xl md:text-4xl xl:text-6xl -tracking-widest text-[#fff]">
           {t("subtitle")}
         </h6>
       </div>
 
-      {/* Scroll indicator */}
-      {/* mouse */}
-      <div className="hidden md:flex absolute bottom-8 left-1/2 animate-bounce">
+      {/* Scroll indicator desktop */}
+      <div className="hidden md:flex absolute bottom-8 left-1/2 z-30 animate-bounce">
         <div className="w-6 h-10 border-2 border-[#2D4A2B] rounded-full flex justify-center">
           <div className="w-1 h-3 bg-[#2D4A2B] rounded-full mt-2 animate-pulse"></div>
         </div>
       </div>
 
-      {/* flecha */}
-     <div className="md:hidden absolute bottom-6 -translate-x-1/2 animate-bounce">
+      {/* Scroll indicator mobile */}
+      <div className="md:hidden absolute bottom-6 left-1/2 z-30 -translate-x-1/2 animate-bounce">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-8 h-8 text-[#2D4A2B]"
@@ -56,6 +59,7 @@ const Hero = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
         </svg>
       </div>
+
     </div>
   );
 };
